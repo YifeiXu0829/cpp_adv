fb916580e0c381a80184c70995a5387cd966f78d
Yifei Xu <yifeix@Yifeis-Air.hsd1.il.comcast.net>
2020-02-01 13:33:23 -0600

    added stack empty check

--- hw4/stack.cpp
+++ hw4/stack.cpp
@@ -20,7 +20,7 @@ template<typename T> class Stack
 
         }
         ~Stack() = default;
-        void push(T a)
+        void push(T a) noexcept
         {
             const std::lock_guard<std::mutex> lock(mutex_);
             std::unique_ptr<Node<T>> node = std::make_unique<Node<T>>(a);
@@ -28,17 +28,22 @@ template<typename T> class Stack
             head_ = std::move(node); // swap between current head resrc and new node resrc, thus current head points to new node, and new node held current head's resrc which is null and will be destructed when new node goes out of scope.
             ++size_;
         }
-        /* [[nodiscard]] */ void pop()
+        [[nodiscard]]  auto pop() noexcept
         {
             const std::lock_guard<std::mutex> lock(mutex_);
             if(size_)
             {
-                //T ret = head_->value_;
+                T ret = head_->value_;
                 head_ = std::move(head_->next_);
                 --size_;
-                //return ret;
+                return ret;
             }
-            //return -1;
+            // undefined behavior ! always check size before you pop
+        }
+
+        bool empty() noexcept
+        {
+            return size_?0:1;
         }
     private:
         int size_;
@@ -52,16 +57,22 @@ int main()
 {
     // how to realize unique_ptr to unique pointer ????
     
-    Stack<int> stack;
+    Stack<std::string> stack;
     
     for(int i=0;i<10;++i)
     {
-        stack.push(i);
+        stack.push(std::to_string(i));
+        std::cout<<i<<" ";
     }
 
-    for(int i=0;i<10;++i)
+    std::cout<<std::endl;
+    for(int i=0;i<11;++i)
     {
-        stack.pop();
+        if(!stack.empty())
+        {
+            std::cout<<stack.pop()<<" ";
+        }
     }
+    std::cout<<std::endl;
     
 }

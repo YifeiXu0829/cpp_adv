2a7565fbd70c8ba50c2c3e625d8d8aaa9834b5f3
Yifei Xu <yifeix@Yifeis-Air.hsd1.il.comcast.net>
2020-02-01 13:13:56 -0600

    finnaly have a feel for std::move

--- hw4/stack.cpp
+++ hw4/stack.cpp
@@ -24,8 +24,8 @@ template<typename T> class Stack
         {
             const std::lock_guard<std::mutex> lock(mutex_);
             std::unique_ptr<Node<T>> node = std::make_unique<Node<T>>(a);
-            node->next_ = std::move(head_);
-            head_ = std::move(node);
+            node->next_ = std::move(head_); // swap resrc between new node next and current head, thus current head points to null, and new node next points to pre head
+            head_ = std::move(node); // swap between current head resrc and new node resrc, thus current head points to new node, and new node held current head's resrc which is null and will be destructed when new node goes out of scope.
             ++size_;
         }
         /* [[nodiscard]] */ void pop()
@@ -50,4 +50,18 @@ template<typename T> class Stack
 
 int main()
 {
+    // how to realize unique_ptr to unique pointer ????
+    
+    Stack<int> stack;
+    
+    for(int i=0;i<10;++i)
+    {
+        stack.push(i);
+    }
+
+    for(int i=0;i<10;++i)
+    {
+        stack.pop();
+    }
+    
 }

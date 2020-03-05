2d8b02ff9f8ae619dc92159c4ed0a875442c21d4
Yifei Xu <yifeix@Yifeis-Air.hsd1.il.comcast.net>
2020-03-04 20:54:00 -0600

    add executables for pointers.cpp and test.cpp(lock-free stack)

--- hw8/test.cpp
+++ hw8/test.cpp
@@ -1,28 +1,10 @@
 #include <iostream>
-
 #include <vector>
-using namespace std;
-class A
-{
-public:
-    A(int val)
-    {
-        _val = val;
-    }
+#include "LockFreeStack.h"
+using std::cout;
+using std::endl;
 
-    int GetVal() { return _val; }
-private:
-    int _val;
-};
-
-void MyFunc(unique_ptr<A> arg)
-{
-    cout << arg->GetVal() << endl;
-}
 
 int main(int argc, char* argv[])
 {
-    unique_ptr<A> ptr = unique_ptr<A>(new A(1234));
-    MyFunc(move(ptr));
-    assert(ptr == nullptr);
 }

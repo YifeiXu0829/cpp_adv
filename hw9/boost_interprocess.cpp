068b6b8307cdcb0e2bdd1b198b4870a263465bb8
Yifei Xu <yifeix@Yifeis-Air.hsd1.il.comcast.net>
2020-03-12 14:50:34 -0500

    boost interprocess test

---
+++ hw9/boost_interprocess.cpp
@@ -0,0 +1,12 @@
+#include <boost/interprocess/managed_shared_memory.hpp>
+#include <boost/interprocess/containers/set.hpp>
+#include <boost/interprocess/allocators/allocator.hpp>
+#include <iostream>
+using namespace boost::interprocess;
+using std::cout;
+using std::endl;
+
+int main()
+{
+    return 0;
+}
